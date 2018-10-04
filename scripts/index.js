6b5eec3b263226eaf137d55d392ae134c4e51167
Allister Rooke <allisterg@gmail.com>
2018-10-04 12:54:15 -0400

    serializeJson working

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,20 @@
 'use strict';
 
+$.fn.extend({
+  serializeJson: function() {
+    const formData = new FormData(this[0]);
+    const o = {};
+    formData.forEach((val, name) => o[name] = val);
+    return JSON.stringify(o);
+  }
+});
+
+
 console.log('workign');
 function handleAdd() {
   $('#js-add').submit(function(event) {
     event.preventDefault();
+    console.log($(event.target).serializeJson());
   });
 }
 
